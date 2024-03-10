e907b250b90535d7888d2b261693823a18694b7f
Spaery <michaelbryan122@gmail.com>
2024-03-10 11:38:07 -0600

    Capability to Add cards to table and update amounts.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,63 @@
-use core::num;
-
-use rusqlite::{Connection,Result};
+use rusqlite::{Connection, Result};
 #[derive(Debug)]
 struct Card {
-    id: u32,
+    edition: String,
+    id: i64,
     name: String,
     foil: bool,
 }
 
 fn main() -> Result<()> {
     let conn = Connection::open("test.db")?;
-    create_table(&conn, "dissension".to_string());
+    create_tables(&conn);
     let co = Card {
+        edition: "Dissension".to_string(),
         id: 107,
         name: "Coiling Oracle".to_string(),
         foil: true,
     };
-    // create_in_db(conn, "dissension".to_string(), co)?;
-    update_db(&conn, "dissension".to_string(), co);
+    update_table(&conn, co);
     Ok(())
 }
 
-fn create_in_db(connection: &Connection, table: String, card: Card) -> Result<()> {
-    connection.execute("INSERT INTO ?1 VALUES (?2, ?3, ?4)",( table, card.id, card.name, card.foil))?;
-    Ok(())
+fn create_tables(connection: &Connection) {
+    match connection.execute("CREATE TABLE IF NOT EXISTS MTG (
+            edition     TEXT NOT NULL,
+            id      INTEGER NOT NULL,
+            name    TEXT NOT NULL,
+            foil    BLOB NOT NULL,
+            quantity INTEGER
+        )", ()) {
+            Ok(ok_value) => (),
+            Err(err) => println!("Error creating table! {}", err),
+        }
+}
+
+fn update_table(connection: &Connection, card: Card){
+    let temp_edition = &card.edition;
+    let temp_name = &card.name;
+    match connection.query_row("SELECT quantity FROM MTG WHERE EXISTS (SELECT id FROM MTG WHERE edition = ?1 AND name = ?2)", (temp_edition, temp_name), |row| row.get::<usize, i64>(0)) {
+        Ok(num_of_card) => { println!("Card found, updating amount!");
+            match connection.execute("UPDATE MTG SET quantity = ?1 WHERE edition = ?2 AND id = ?3", (num_of_card + 1, card.edition, card.id)){
+                Ok(ok_value) => println!("{}",ok_value),
+                Err(err) => println!("Update table error!: {}", err)
+            }; 
+        }
+        Err(err) => {
+            println!("Card not found, adding!");
+            create_in_table(connection, card);
+        }
+    };
+}
+
+fn create_in_table(connection: &Connection, card: Card) {
+    match connection.execute("INSERT INTO MTG VALUES (?1, ?2, ?3, ?4, ?5)",( card.edition, card.id, card.name, card.foil, 1)) {
+        Ok(ok_value) => println!("{}",ok_value),
+        Err(err) => println!("Entry creation error: {}",err)
+    };
 }
 
 fn delete_from_db(connection: &Connection, card: Card) -> Result<()>{
 
     Ok(())
-}
-
-fn update_db(connection: &Connection,table: String, card: Card){
-    match connection.execute("SELECT 1 FROM ?1 WHERE name = ?2", (table, card.name)) {
-        Ok(num_of_card) => println!("{} selected", num_of_card),
-        Err(error) => println!("Update database error! {}", error)
-    };
-}
-
-fn create_table(connection: &Connection, table: String) {
-    match connection.execute("CREATE TABLE ?1 (
-            id      INTEGER PRIMARY KEY,
-            name    TEXT NOT NULL,
-            foil    BLOB
-        )", [table]) {
-            Ok(updated) => println!("{} updated", updated),
-            Err(err) => println!("Create table error! {}", err),
-        }
 }
